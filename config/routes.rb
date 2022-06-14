fdccf16af89996f0c8a3bce815675dd53f7a1018
umeharaharuki <jasper1116lopez@icloud.com>
2022-06-14 12:14:48 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
   devise_for :users
   get 'prototypes/index'
   root to: "prototypes#index"
   resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
   resources :comments, only: :create
   end
-  resources :users, only: :show
+  resources :users, only: :show do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
